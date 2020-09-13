245ae9ed38421708ec2fa46f2add1ade4cc845d2
Anthony <anthgalea1@gmail.com>
2020-09-13 10:10:30 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $(".correctAnswer").on("click", function(){
+        alert("Correct!");
+    });
+
+    $(".wrongAnswer").on("click", function(){
+        alert("Incorrect");
+    });
+});
+
+   
+
+
+//This is a trivia game that will have 6 multiple choice questions.
+
+//1. Create a landing page that welcomes the user and has a start button to begin the quiz.
+
+//2. The correct answer will become green while the incorrect answers will be red after an option is clicked. (add a submit button and radio buttons for multiple choice options)
+
+//3. Put the correct answer in an object/array, and hard code the rest of the quiz in html
+
+
+/*$(document).ready(function(){
+
+    /*$(".correctAnswer").addClass("click", function (){
+        To make an option turn red/green
+    });
+
+});
+*/
