cb355104f521bf4427614338e75ec250bda264fd
Anthony <anthgalea1@gmail.com>
2020-09-13 15:30:39 -0400

    second commit

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
+const score = {
+    correct: 0,
+    wrong:0
+}
+
 $(document).ready(function(){
-    $(".correctAnswer").on("click", function(){
-        alert("Correct!");
-    });
-
-    $(".wrongAnswer").on("click", function(){
-        alert("Incorrect");
+   $("form").on("submit", function(event){
+        event.preventDefault();
+        handleSubmit();
     });
 });
 
-   
+function handleSubmit(){
+//add class to correct answer to turn it green on submit.
+
+   if($("input:radio[class='correctAnswer']").is(":checked")) {
+        $(".correctAnswer").next().addClass("green");
+        console.log('correct'); 
+        score.correct++;
 
 
-//This is a trivia game that will have 6 multiple choice questions.
-
-//1. Create a landing page that welcomes the user and has a start button to begin the quiz.
-
-//2. The correct answer will become green while the incorrect answers will be red after an option is clicked. (add a submit button and radio buttons for multiple choice options)
-
-//3. Put the correct answer in an object/array, and hard code the rest of the quiz in html
+        //to turn correct answers green
+   };
 
 
-/*$(document).ready(function(){
+   if($("input:radio[class='wrongAnswer']").is(":checked")) {
+    $(".wrongAnswer").next().addClass("red");
+    console.log(score.wrong);
+    //to turn wrong answers red
+    score.wrong++ ;
+    //to add up wrong answers
+    } 
 
-    /*$(".correctAnswer").addClass("click", function (){
-        To make an option turn red/green
-    });
 
-});
+
+   if($("input:radio[class='wrongAnswer']").is(":checked")) {
+    
+    } 
+
+
+};
+
+
+    //bracket colourizer
+
+ /*   $(".wrongAnswer").on("click", function(){
+        alert("Incorrect");*/
+   // });
+  // $(".correctAnswer").on("click",function(){
+      //  $(".correctAnswer").addClass("green");
+   // });
+/*
+This is a trivia game that will have 6 multiple choice questions.
+
+1. Create a landing page that welcomes the user and has a start button to begin the quiz.
+
+2. The correct answer will become green while the incorrect answers will be red after an option is clicked. (add a submit button and radio buttons for multiple choice options)
+
+3. Put the correct answer in an object/array, and hard code the rest of the quiz in html
 */
